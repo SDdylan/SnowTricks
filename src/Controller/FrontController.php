d8b95da5685d9f7f5067fed3922f0fbe6e8b1307
Dylan SARDI <dylan.sardi@gmail.com>
2022-02-28 11:46:04 +0100

    Added homepage controller. Added bootstrap theme and twig template. Begin homepage layout

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/front", name="front")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('front/home.html.twig');
+    }
+}
