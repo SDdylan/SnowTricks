08c8dd61f6a4414fac3b133f39103c8957f6510b
Dylan SARDI <dylan.sardi@gmail.com>
2022-03-02 17:06:22 +0100

    Added package for asset in home controller Home page modified, added images.

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
+
+
     /**
      * @Route("/front", name="front")
      */
@@ -23,6 +27,11 @@ class FrontController extends AbstractController
      */
     public function home()
     {
-        return $this->render('front/home.html.twig');
+        //Donnée a utiliser en Assert une fois le soucis réglé
+        $package = new Package(new EmptyVersionStrategy());
+        $img = $package->getUrl('img/img2.jpg');
+        $css = $package->getUrl('img/img2.jpg');
+
+        return $this->render('front/home.html.twig', ['img' => $img, 'css' => $css]);
     }
 }
