4304fa391a06e61f0e08ccdefe573e0af3e8ff37
Dylan SARDI <dylan.sardi@gmail.com>
2022-03-10 15:50:36 +0100

    Deleted old form, @TODO : addFlash() in RegistrationController.php @TODO : Changes the routes to redirect after registration.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -58,7 +58,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_security');
+            return $this->redirectToRoute('/');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -95,6 +95,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('/');
     }
 }
